ac8dce3a33c11f17f225072b58205161c91bba99
hongwei1 <hongwei@tesobe.com>
2018-12-11 14:06:00 +0100

    set the log to Debug to test all the Oauth process

--- hello_obp.py
+++ hello_obp.py
@@ -9,22 +9,12 @@ Configure settings.py to suit your needs.
 from pprint import pprint
 
 from oauth_dance import get_api
-from settings import API_BASE_URL, API_USER_URL
-
+from settings import API_USER_URL
+import logging
 
 def say_hello():
     """Says hello to the API"""
     api = get_api()
-    response = api.get(API_BASE_URL)
-    status_code = response.status_code
-    print('Status code: {}'.format(status_code))
-    if status_code == 200:
-        print('Success JSON:')
-        pprint(response.json())
-    else:
-        print(response.text)
-
-
     
     """Get the current authenticated User """
     response = api.get(API_USER_URL)
@@ -38,4 +28,5 @@ def say_hello():
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,)
     say_hello()
